80dc08c96c7f0cdb9d3ccb231996b8112ce6f2ed
patelanuj21 <patel.anuj21@gmail.com>
2015-11-28 00:31:30 -0800

    updated polls functionality

--- public/controller/pollsController.js
+++ public/controller/pollsController.js
@@ -74,6 +74,63 @@ angular
     	}
     	
     	
+    	
+    	//This function will display the form to add new poll response
+    	$scope.pollResp = false;
+    	$scope.pollResponse = function(){
+        	console.log("Allowing to respond to this poll");
+    		$http.get('/polls').success(function(response){
+    			$scope.pollResp=true;
+        		
+        	});
+        }
+    	
+    	
+    		
+    	//Radio Button for 
+    	  $scope.data = {
+    		      group1 : 'Banana',
+    		      group2 : '2',
+    		      group3 : 'avatar-1'
+    		    };
+    		    $scope.avatarData = [{
+    		        id: "avatars:svg-1",
+    		        title: 'avatar 1',
+    		        value: 'avatar-1'
+    		      },{
+    		        id: "avatars:svg-2",
+    		        title: 'avatar 2',
+    		        value: 'avatar-2'
+    		      },{
+    		        id: "avatars:svg-3",
+    		        title: 'avatar 3',
+    		        value: 'avatar-3'
+    		    }];
+    		    $scope.radioData = [
+    		      { label: '1', value: 1 },
+    		      { label: '2', value: 2 },
+    		      { label: '3', value: '3', isDisabled: true },
+    		      { label: '4', value: '4' }
+    		    ];
+    		    $scope.submit = function() {
+    		      alert('submit');
+    		    };
+    		    $scope.addItem = function() {
+    		      var r = Math.ceil(Math.random() * 1000);
+    		      $scope.radioData.push({ label: r, value: r });
+    		    };
+    		    $scope.removeItem = function() {
+    		      $scope.radioData.pop();
+    		    };
+
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
     
     
     /**
